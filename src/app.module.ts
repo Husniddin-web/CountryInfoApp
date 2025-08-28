11544e721b86f7ce59a52175d191ee95aad79440
Husnidin <husniddinsalohiddinov1974@gmail.com>
2025-08-28 11:09:36 +0500

    feat:add modules calendar and countries

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from './config';
+import { CountriesModule } from './modules/countries/countries.module';
+import { CalendarModule } from './modules/calendar/calendar.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(config.mongoUri),
+    CountriesModule,
+    CalendarModule,
   ],
   controllers: [],
   providers: [],
